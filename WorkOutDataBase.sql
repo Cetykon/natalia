c3a38d2aa7546e347df34c00c3ee85fa16acd8ec
Jesus Aguilar <aguilarandradejesus@gmail.com>
2024-12-29 03:32:10 -0500

    Added Database Base Version File

---
+++ WorkOutDataBase.sql
@@ -0,0 +1,84 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.3.0-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.5.0.6677
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for workout
+CREATE DATABASE IF NOT EXISTS `workout` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `workout`;
+
+-- Dumping structure for table workout.exercises
+CREATE TABLE IF NOT EXISTS `exercises` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `workout` varchar(50) DEFAULT NULL,
+  `reps` varchar(50) DEFAULT NULL,
+  `sets` varchar(50) DEFAULT NULL,
+  `weight` varchar(50) DEFAULT NULL,
+  `lastupdate` varchar(50) DEFAULT NULL,
+  `setday` varchar(50) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table workout.exercises: ~29 rows (approximately)
+INSERT INTO `exercises` (`id`, `workout`, `reps`, `sets`, `weight`, `lastupdate`, `setday`, `image`) VALUES
+	(2, 'Leg press', '12', '3', '230ib', '10/20/22', 'Monday', 'LegPress.jpg'),
+	(5, 'rear delts', '12', '3', '50lb', '10/22/23', 'Wednesday', NULL),
+	(6, 'bench press', '12', '3', '50lb', '10/21/21', 'Thursday', 'BenchPress.jpg'),
+	(7, 'calf Machine', '12', '3', '110ib', '10/21/21', 'Friday', NULL),
+	(8, 'crunches', '50', '2', 'body weight', '10/21/21', 'monday', NULL),
+	(9, 'push-ups', '20', '3', 'body weight', '10/21/21', 'Monday', NULL),
+	(10, 'lunges', '15', '4', 'body weight', '10/21/21', 'Wednesday', NULL),
+	(11, 'planks', '1 minute', '3', 'body weight', '10/21/21', 'Friday', NULL),
+	(12, 'bicep curls', '12', '4', '30ib', '10/21/21', 'Tuesday', NULL),
+	(13, 'squats', '25', '3', 'body weight', '10/21/21', 'Thursday', NULL),
+	(14, 'pull-ups', '10', '4', 'body weight', '10/21/21', 'Saturday', NULL),
+	(15, 'burpees', '15', '3', 'body weight', '10/21/21', 'Sunday', NULL),
+	(16, 'leg raises', '20', '3', 'body weight', '10/21/21', 'Thursday', NULL),
+	(17, 'dips', '12', '3', 'body weight', '10/21/21', 'Monday', NULL),
+	(18, 'Russian twists', '20', '3', 'body weight', '10/21/21', 'Wednesday', NULL),
+	(19, 'Mountain climbers', '30 seconds', '4', 'body weight', '10/21/21', 'Friday', NULL),
+	(20, 'Deadlifts', '8', '3', '170lb', '10/21/21', 'Sunday', NULL),
+	(21, 'Seated leg curls', '12', '3', '90lb', '10/21/21', 'Thursday', NULL),
+	(22, 'Shoulder presses', '10', '4', '25lb', '10/21/21', 'Wednesday', NULL),
+	(23, 'Chin-ups', '8', '3', 'body weight', '10/21/21', 'Saturday', NULL),
+	(25, 'Incline bench press', '10', '4', '155lb', '10/21/21', 'Sunday', NULL),
+	(27, 'Barbell Shrugs', '15', '3', 'barbell', '10/21/21', 'Friday', NULL),
+	(28, 'Front Raises', '12', '3', 'dumbbells', '10/21/21', 'Monday', NULL),
+	(29, 'Side Leg Raises', '15', '4', 'body weight', '10/21/21', 'Sunday', NULL),
+	(30, 'Step-ups', '12', '3', 'body weight', '10/21/21', 'Saturday', NULL),
+	(31, 'Hammer Curls', '10', '3', '33lb', '10/21/21', 'Thursday', NULL),
+	(32, 'Bicycle Crunches', '30', '4', 'body weight', '10/21/21', 'Tuesday', NULL),
+	(33, 'Tricep Dips', '15', '3', 'body weight', '10/21/21', 'Friday', NULL),
+	(34, 'Typing', '200', '1', 'hands', '10/22/23', 'monday', NULL),
+	(38, 'test', '5', '3', '20lb', '12/12/23', 'monday', NULL);
+
+-- Dumping structure for table workout.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) NOT NULL DEFAULT 0,
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Dumping data for table workout.users: ~1 rows (approximately)
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+	(1, 'admin', 'admin');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
